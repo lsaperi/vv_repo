fa52859822cb319c14d377cbafb7126024b913f4
lsaperi <lsaperi>
2025-05-28 17:25:04 -0300

    34; TaskID: task2

--- TABLEs/Creation-Scripts/new_table.sql
+++ TABLEs/Creation-Scripts/new_table.sql
@@ -1,4 +1,6 @@
 CREATE TABLE `new_table` (
   `idnew_table` int NOT NULL,
+  `new_tablecol` varchar(45) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idnew_table`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
